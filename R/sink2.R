5f0dea18c00545e852aab4efeb7a46b25894f34e
Tingting Zhan <tingtingzhan@gmail.com>
2026-02-17 19:08:20 -0500

    tzh

--- R/sink2.R
+++ R/sink2.R
@@ -2,9 +2,13 @@
 
 #' @title Sink an Object to a File
 #' 
+#' @description
+#' \link[base]{sink} an R object to a local file.
+#' 
+#' 
 #' @param x an R object
 #' 
-#' @param file,... parameters of function \link[base]{sink}
+#' @param file,... parameters of the function \link[base]{sink}
 #' 
 #' @note
 #' Too bad the function \link[base]{sink} is not an `S3` generic.
@@ -18,12 +22,17 @@ sink2 <- function(x, file, ...) UseMethod(generic = 'sink2')
 #' @export sink2.Bibtex
 #' @export
 sink2.Bibtex <- function(x, file, ...) {
+  
   if (file.exists(file)) file.remove(file) # without warning
-  file |> file.create()
+  
+  file |> 
+    file.create()
+  
   sink(file = file, ...)
   x |> 
     print() # utils:::print.Bibtex
   sink()
+  
 }
 
 
@@ -32,7 +41,7 @@ sink2.Bibtex <- function(x, file, ...) {
 #' @export
 sink2.bibentry <- function(x, ...) {
   x |> 
-    toBibtex() |> 
+    toBibtex() |> # utils:::toBibtex.bibentry
     sink2.Bibtex(...)
 }
 
